2cfbb4ba408d858e3ce59c9fea4788ca0ad13ade
Iyonissio Daniel Sitoe <57901563+Iyonissio@users.noreply.github.com>
2021-07-16 14:41:45 +0200

    commit new ptoject

--- lib/pages/home/widgets/my_app_bar.dart
+++ lib/pages/home/widgets/my_app_bar.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+// '',
 class MyAppBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -9,8 +10,29 @@ class MyAppBar extends StatelessWidget {
           height: MediaQuery.of(context).padding.top,
         ),
         Container(
-          color: Colors.red,
-          height: 200,
+          color: Colors.purple[800],
+          height: 120,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Image.network(
+                'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f7/Nubank_logo_2021.svg/1200px-Nubank_logo_2021.svg.png',
+                height: 35,
+                color: Colors.white,
+              ),
+              SizedBox(
+                width: 10,
+              ),
+              Text(
+                'Iyonissio',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18,
+                ),
+              ),
+            ],
+          ),
+          
         )
       ],
     );
